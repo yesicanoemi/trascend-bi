891edeca87dc6895e9f087ae13b2bb4ec85c994d
lmd411 <lmd411@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-13 23:31:43 +0000

    Agregacion de atributos a Cargo

--- trunk/trascend-bi/src/Core/LogicaNegocio/Entidades/Cargo.cs
+++ trunk/trascend-bi/src/Core/LogicaNegocio/Entidades/Cargo.cs
@@ -8,6 +8,10 @@ namespace Core.LogicaNegocio.Entidades
     public class Cargo
     {
         private String nombre;
+        private String descripcion;
+        private float sueldo_minimo;
+        private float sueldo_maximo;
+        private DateTime vigencia;
 
         public virtual String Nombre
         {
@@ -18,6 +22,50 @@ namespace Core.LogicaNegocio.Entidades
             set
             {
                 nombre = value;
+            }
+        }
+        public virtual String Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+            set
+            {
+                descripcion = value;
+            }
+        }
+        public virtual float SueldoMinimo
+        {
+            get
+            {
+                return sueldo_minimo;
+            }
+            set
+            {
+                sueldo_minimo = value;
+            }
+        }
+        public virtual float SueldoMaximo
+        {
+            get
+            {
+                return sueldo_maximo;
+            }
+            set
+            {
+                sueldo_maximo = value;
+            }
+        }
+        public virtual DateTime Vigencia
+        {
+            get
+            {
+                return vigencia;
+            }
+            set
+            {
+                vigencia = value;
             }
         }
     }
