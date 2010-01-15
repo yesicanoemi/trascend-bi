e7a1d8991ac2ce3296b64ff43ced6e8b5c34953e
cemelc <cemelc@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-15 15:54:51 +0000

    Actualizando de nuevo

---
+++ trunk/trascend-bi/src/Core/LogicaNegocio/Entidades/Telefono.cs
@@ -0,0 +1,82 @@
+﻿namespace Core.LogicaNegocio.Entidades
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Telefono
+    {
+
+        private int codigoArea;
+        private int numero;
+        private string tipo;
+        private string rifCliente;
+        private string rifContacto;
+
+
+
+        public virtual int Codigoarea
+        {
+            get
+            {
+                return codigoArea;
+            }
+
+            set
+            {
+                codigoArea = value;
+            }
+        }
+
+        public virtual int Numero
+        {
+            get
+            {
+                return numero;
+            }
+
+            set
+            {
+                numero = value;
+            }
+        }
+
+        public virtual string Tipo
+        {
+            get
+            {
+                return tipo;
+            }
+
+            set
+            {
+                tipo = value;
+            }
+        }
+
+        public virtual string Rifcliente
+        {
+            get
+            {
+                return rifCliente;
+            }
+
+            set
+            {
+                rifCliente = value;
+            }
+        }
+        public virtual string Rifcontacto
+        {
+            get
+            {
+                return rifContacto;
+            }
+
+            set
+            {
+                rifContacto = value;
+            }
+        }
+
+    }
+}
