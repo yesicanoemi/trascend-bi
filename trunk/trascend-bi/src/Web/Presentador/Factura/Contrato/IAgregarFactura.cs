1fe37ce79fa02031082b9a1e23f16bff30c185fc
luis.gab <luis.gab@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-15 23:44:03 +0000

    Modificación MVP - Desarrollo9

--- trunk/trascend-bi/src/Web/Presentador/Factura/Contrato/IAgregarFactura.cs
+++ trunk/trascend-bi/src/Web/Presentador/Factura/Contrato/IAgregarFactura.cs
@@ -10,23 +10,16 @@ namespace Presentador.Factura.Contrato
     {
         MultiView MultiViewPropuesta { get; set; }
      //   TextBox ProyectoAsociado { get; set; }
-        Label LNombrePropuesta { get; set; }
-
+      
         #region InformacionPropuesta
 
-        Label LDescripcion { get; set; }
+       
         TextBox Descripcion { get; set; }
-        Label LMontoTotal { get; set; }
         TextBox MontoTotal { get; set; }
-        Label LMontoCancelado { get; set; }
         TextBox MontoCancelado { get; set; }
-        Label LTotalCancelado { get; set; }
         TextBox TotalCancelado { get; set; }
-        Label LPorcentajeCancelado { get; set; }
         TextBox PorcentajeCancelado { get; set; }
-        Label LMontoFaltante { get; set; }
         TextBox MontoFaltante { get; set; }
-        Label LPorcentajeFaltante { get; set; }
         TextBox PorcentajeFaltante { get; set; }
 
         #endregion
@@ -35,13 +28,9 @@ namespace Presentador.Factura.Contrato
 
         #region Facturacion
 
-        Label LMontoPagar { get; set; }
         TextBox MontoPagar { get; set; }
-        Label LPorcentajePagar { get; set; }
         TextBox PorcentajePagar { get; set; }
-        Label LCodigoFactura { get; set; }
         TextBox CodigoFactura { get; set; }
-        Label LTituloFactura { get; set; }
         TextBox TituloFactura { get; set; }
         DropDownList EstadoFactura { get; set; }
         TextBox FechaPagoFact { get; set; }
--- trunk/trascend-bi/src/Web/Site1/Paginas/Facturas/AgregarFacturas.aspx.cs
+++ trunk/trascend-bi/src/Web/Site1/Paginas/Facturas/AgregarFacturas.aspx.cs
@@ -25,31 +25,13 @@ public partial class Paginas_Facturas_AgregarFacturas : System.Web.UI.Page,IAgre
     }
     */
 
-    public Label LNombrePropuesta
-    {
-        get { return this.uxLabelNomProp; }
-        set { this.uxLabelNomProp = value; }
-    }
-
-
+    
     #region InformacionPropuesta
-
-    public Label LDescripcion
-    {
-        get { return this.uxLabelDescProp; }
-        set { this.uxLabelDescProp = value; }
-    }
 
     public TextBox Descripcion
     {
         get { return this.uxDescProp; }
         set { this.uxDescProp = value; }
-    }
-
-    public Label LMontoTotal
-    {
-        get { return this.uxLabelMontoTotal; }
-        set { this.uxLabelMontoTotal = value; }
     }
 
     public TextBox MontoTotal
@@ -58,22 +40,10 @@ public partial class Paginas_Facturas_AgregarFacturas : System.Web.UI.Page,IAgre
         set { this.uxMontoTotal = value; }
     }
 
-    public Label LMontoCancelado
-    {
-        get { return this.uxLabelMontoCancelado; }
-        set { this.uxLabelMontoCancelado = value; }
-    }
-
     public TextBox MontoCancelado
     {
         get { return this.uxMontoCancelado; }
         set { this.uxMontoCancelado = value; }
-    }
-
-    public Label LTotalCancelado
-    {
-        get { return this.uxLabelTotalCancelado; }
-        set { this.uxLabelTotalCancelado = value; }
     }
 
     public TextBox TotalCancelado
@@ -82,34 +52,16 @@ public partial class Paginas_Facturas_AgregarFacturas : System.Web.UI.Page,IAgre
         set { this.uxTotalCancelado = value; }
     }
 
-    public Label LPorcentajeCancelado
-    {
-        get { return this.uxLabelPorcCancelado; }
-        set { this.uxLabelPorcCancelado = value; }
-    }
-
     public TextBox PorcentajeCancelado
     {
         get { return this.uxPorcentajeCancelado; }
         set { this.uxPorcentajeCancelado = value; }
     }
 
-    public Label LMontoFaltante
-    {
-        get { return this.uxLabelMontoFalt; }
-        set { this.uxLabelMontoFalt = value; }
-    }
-
     public TextBox MontoFaltante
     {
         get { return this.uxMontoFaltante; }
         set { this.uxMontoFaltante = value; }
-    }
-
-    public Label LPorcentajeFaltante
-    {
-        get { return this.uxLabelPorcFalt; }
-        set { this.uxLabelPorcFalt = value; }
     }
 
     public TextBox PorcentajeFaltante
@@ -123,22 +75,10 @@ public partial class Paginas_Facturas_AgregarFacturas : System.Web.UI.Page,IAgre
 
     #region Facturacion
 
-    public Label LMontoPagar
-    {
-        get { return this.uxLabelMontoPagar; }
-        set { this.uxLabelMontoPagar = value; }
-    }
-
     public TextBox MontoPagar
     {
         get { return this.uxMontoPagar; }
         set { this.uxMontoPagar = value; }
-    }
-
-    public Label LPorcentajePagar
-    {
-        get { return this.uxLabelPorcPagar; }
-        set { this.uxLabelPorcPagar = value; }
     }
 
     public TextBox PorcentajePagar
@@ -147,22 +87,10 @@ public partial class Paginas_Facturas_AgregarFacturas : System.Web.UI.Page,IAgre
         set { this.uxPorcentajePagar = value; }
     }
 
-    public Label LCodigoFactura
-    {
-        get { return this.uxLabelCodFactura; }
-        set { this.uxLabelCodFactura = value; }
-    }
-
     public TextBox CodigoFactura
     {
         get { return this.uxCodigoFactura; }
         set { this.uxCodigoFactura = value; }
-    }
-
-    public Label LTituloFactura
-    {
-        get { return this.uxLabelTituloFact; }
-        set { this.uxLabelTituloFact = value; }
     }
 
     public TextBox TituloFactura
