40afb2be9c222344bd38fd67d6d0b60da5fd43f9
cemelc <cemelc@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-15 18:40:58 +0000

    FabricaTelefono

---
+++ trunk/trascend-bi/src/Core/LogicaNegocio/Fabricas/FabricaTelefono.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.LogicaNegocio.Entidades;
+using Core.LogicaNegocio.Comandos.ComandoContacto;
+
+namespace Core.LogicaNegocio.Fabricas
+{
+    public class FabricaTelefono
+    {
+                
+
+        public static TelefonoCelular CrearTelefonoCelular(TelefonoCelular telefonocel)
+        {
+            
+            return telefonocel= new TelefonoCelular();
+        }
+        public static TelefonoTrabajo CrearTelefonoTrabajo(TelefonoTrabajo telefonotrabajo)
+        {
+            return telefonotrabajo = new TelefonoTrabajo();
+        }
+   
+    }
+}
