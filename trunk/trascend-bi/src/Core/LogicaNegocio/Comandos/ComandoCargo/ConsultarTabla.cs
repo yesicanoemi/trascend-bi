88ba6155f29e5ba4b3e61fe312f7f26433ab06e8
lmd411 <lmd411@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-17 12:56:05 +0000

    Comando ConsultarTabla

---
+++ trunk/trascend-bi/src/Core/LogicaNegocio/Comandos/ComandoCargo/ConsultarTabla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.LogicaNegocio.Entidades;
+using Core.AccesoDatos.SqlServer;
+
+namespace Core.LogicaNegocio.Comandos.ComandoCargo
+{
+    public class ConsultarTabla : Comando<Cargo>
+    {
+        public ConsultarTabla()
+        { 
+        }
+
+        public List<Cargo> Ejecutar()
+        {
+            CargoSQLServer bd = new CargoSQLServer();
+            List<Cargo> ListaCargos = new List<Cargo>();
+            IList<Entidad> ListaEntidades = bd.ConsultarCargos();
+            
+            for (int i = 0; i < ListaEntidades.Count; i++)
+            {
+                ListaCargos.Add((Cargo)ListaEntidades[i]);
+            }
+            return ListaCargos;
+        }
+    }
+}
