c5c4e7b6047c2f410c211019070c65f9ec274b6d
lmd411 <lmd411@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-17 13:28:18 +0000

    Presentador de Reporte de facturas cobradas

---
+++ trunk/trascend-bi/src/Web/Presentador/Reportes/Vistas/ReporteFacturasCobradasPresenter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Presentador.Reportes.Contrato;
+using Core.LogicaNegocio.Comandos.ComandoReporte;
+
+namespace Presentador.Reportes.Vistas
+{
+    public class ReporteFacturasCobradasPresenter 
+    {
+        private IReporteFacturasPorCobrar _vista;
+
+        public ReporteFacturasCobradasPresenter(IReporteFacturasPorCobrar laVista)
+        {
+            _vista = laVista;
+        }
+
+        public void CargarGrid()
+        {
+
+            _vista.Grid.DataSource = null;
+            _vista.Grid.DataBind();
+
+            Core.LogicaNegocio.Comandos.ComandoReporte.ConsultarFacturasPorEstado ComandoConsultarFacturas;
+
+            ComandoConsultarFacturas = Core.LogicaNegocio.Fabricas.FabricaComandosReporte.CrearComandoConsultarFacturasPorEstado
+                                                                (Convert.ToDateTime(_vista.FechaInicio.Text),
+                                                                Convert.ToDateTime(_vista.FechaFin.Text),
+                                                                true);
+
+            IList<Core.LogicaNegocio.Entidades.Factura> lista = ComandoConsultarFacturas.Ejecutar();
+
+
+
+            /*        Core.AccesoDatos.SqlServer.FacturaSQLServer bd = new Core.AccesoDatos.SqlServer.FacturaSQLServer();
+
+                    IList<Core.LogicaNegocio.Entidades.Factura> lista = bd.ConsultarFacturasxEstado(Convert.ToDateTime(_vista.FechaInicio.Text), 
+                                                                        Convert.ToDateTime(_vista.FechaFin.Text), 
+                                                                        false); */
+
+            if (lista != null)
+            {
+                _vista.Grid.DataSource = lista;
+                _vista.Grid.DataBind();
+            }
+        }
+    }
+}
