ab5ffd41e3d045a39e0c819daa38f8300a911a42
jesusviloriag <jesusviloriag@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-17 00:37:24 +0000

    Procedure cambiado porque era muy corto el varchar

--- trunk/trascend-bi/StoredProcedures/ConsultarFacturasNomPro.sql
+++ trunk/trascend-bi/StoredProcedures/ConsultarFacturasNomPro.sql
@@ -1,5 +1,8 @@
-CREATE PROCEDURE [dbo].[ConsultarFacturaNomPro]
-@titulo varchar(20)
+set ANSI_NULLS ON
+set QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[ConsultarFacturaNomPro]
+@titulo varchar(50)
 AS
 BEGIN
 SELECT f.* From Factura f where f.IdPropuesta in (select p.idpropuesta from propuesta p where p.titulo = @titulo )
