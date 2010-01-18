00f6b742033a87207e7181a75ccf7800d2fd32ef
alexrom7@gmail.com <alexrom7@gmail.com@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-18 23:14:21 +0000

    Arreglando los namespaces de Excepciones

--- trunk/trascend-bi/src/Core/LogicaNegocio/Excepciones/Facturas/LogicaNegocio/IngresarFacturaLNException.cs
+++ trunk/trascend-bi/src/Core/LogicaNegocio/Excepciones/Facturas/LogicaNegocio/IngresarFacturaLNException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Core.LogicaNegocio.Excepciones.Facturas.Logica_de_Negocios
+namespace Core.LogicaNegocio.Excepciones.Facturas.LogicaNegocio
 {
     class IngresarFacturaLNException
     {
