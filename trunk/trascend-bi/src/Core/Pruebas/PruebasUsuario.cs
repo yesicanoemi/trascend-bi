5ca58ee8501ac43c5b854fc45da0fd728a44b714
dinangela.rojas <dinangela.rojas@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-20 10:42:06 +0000

    Desarrollo 3
    Prueba modificar

--- trunk/trascend-bi/src/Core/Pruebas/PruebasUsuario.cs
+++ trunk/trascend-bi/src/Core/Pruebas/PruebasUsuario.cs
@@ -13,7 +13,10 @@ namespace Core.Pruebas
     [TestFixture]
     class PruebasUsuario
     {
+        ///Prueba Para Consultar Usuario
+       
         [Test]
+        
         public void TestConsultarUsuario()
         {
             Usuario usuario = new Usuario();
@@ -45,41 +48,64 @@ namespace Core.Pruebas
             Assert.AreEqual(Login,usuario.Login);
         }
 
+
+        //Prueba Para Agregar Usuario 
+
         [Test]
+
         public void TestAgregarUsuario()
         {
             Usuario usuario = new Usuario();
 
             usuario.Login = "UsuarioPrueba";
+            
             usuario.Password = "123456";
+            
             usuario.Status = "Activo";
+            
             usuario.Cedula = 13776256;
 
             IList<Permiso> listado = new List<Permiso>();
+            
             Permiso permiso = new Permiso();
+            
             permiso.IdPermiso = 1;
+            
             listado.Add(permiso);
 
             UsuarioSQLServer bd = new UsuarioSQLServer();
-                bd.AgregarUsuario(usuario);
+            
+            bd.AgregarUsuario(usuario);
+            
             IList<Usuario> listadoUsu = new List<Usuario>();
-                listadoUsu = bd.ConsultarUsuario(usuario);
+            
+            listadoUsu = bd.ConsultarUsuario(usuario);
 
             Assert.AreEqual(listadoUsu[0].Login, usuario.Login);
         }
 
+
+
+
+        //Prueba Para Modificar Usuario
+
         [Test]
+        
         public void TestModificarUsuario()
         {
             Usuario usuario = new Usuario();
 
             Usuario usuarioTest = new Usuario();
 
+            Permiso permiso = new Permiso();
+
             UsuarioSQLServer bd = new UsuarioSQLServer();
 
             IList<Usuario> listadoUsuarios = new List<Usuario>();
 
             IList<Permiso> listadoPermisos = new List<Permiso>();
+
+            IList<Permiso> listadoPermisosTest = new List<Permiso>();
 
             //consulta de usuario y sus permisos
             listadoUsuarios = bd.ConsultarUsuario(usuario);
@@ -88,37 +114,29 @@ namespace Core.Pruebas
 
             listadoPermisos = bd.ConsultarPermisos(listadoUsuarios[0]);
 
-           /* if (listadoPermisos[0] != null)
-            {
-                //asignacion del permiso al usuario
-                usuario.PermisoUsu[0].IdPermiso = listadoPermisos[0].IdPermiso;
-
-                //modificacion del permiso
-                usuario.PermisoUsu[0].IdPermiso = 5;
-
-            }
-
-            else
-            {
-                //modificacion del permiso
-                usuario.PermisoUsu[0].IdPermiso = 5;
-            
-            }
-*/
-            if (usuario.Status.CompareTo("Inactivo") > 0)
+            if (usuario.Status.CompareTo("Inactivo") >= 0)
             {
                 usuario.Status = "Activo";
             }
 
+            permiso.IdPermiso = 6;
+
+            listadoPermisosTest.Add(permiso);
+            
+            usuario.PermisoUsu = listadoPermisosTest;
+            
             bd.ModificarUsuario(usuario);
 
             //consulta de permisos y de usuario nuevamente
-            listadoPermisos = bd.ConsultarPermisos(listadoUsuarios[0]);
+
             listadoUsuarios = bd.ConsultarUsuario(usuario);
+
+            listadoPermisos = bd.ConsultarPermisos(listadoUsuarios[0]);
 
             usuarioTest = listadoUsuarios[0];
          
-            Assert.AreEqual(5,listadoPermisos[0].IdPermiso);
+            Assert.AreEqual(6,listadoPermisos[0].IdPermiso);
+
             Assert.AreEqual(usuario.Status, usuarioTest.Status);
         }
 
