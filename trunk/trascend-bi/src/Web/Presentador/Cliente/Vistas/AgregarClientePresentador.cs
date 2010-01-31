7d0119770df8ff747d74f60994b3f0ae8ba9f582
alexrom7@gmail.com <alexrom7@gmail.com@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-01-31 21:49:56 +0000

    Desarrollo 9: Clientes

--- trunk/trascend-bi/src/Web/Presentador/Cliente/Vistas/AgregarClientePresentador.cs
+++ trunk/trascend-bi/src/Web/Presentador/Cliente/Vistas/AgregarClientePresentador.cs
@@ -16,65 +16,67 @@ namespace Presentador.Cliente.Vistas
 {
     public class AgregarClientePresentador
     {
-        
-            private IAgregarCliente _vista;
-            private string campoVacio = "";
 
-            #region Constructor
-            public AgregarClientePresentador(IAgregarCliente vista)
+        private IAgregarCliente _vista;
+        private string campoVacio = "";
+
+        #region Constructor
+        public AgregarClientePresentador(IAgregarCliente vista)
+        {
+            _vista = vista;
+        }
+        #endregion
+
+        #region metodos
+
+        public void IngersarCliente()
+        {
+            Core.LogicaNegocio.Entidades.Cliente cliente = new Core.LogicaNegocio.Entidades.Cliente();
+            try
             {
-                _vista = vista;
+                cliente.Direccion = new Direccion();
+
+                //cliente.Telefono = new TelefonoTrabajo();
+
+                cliente.Rif = _vista.rifCliente.Text;
+
+                cliente.Nombre = _vista.NombreCliente.Text;
+
+                cliente.Direccion = new Core.LogicaNegocio.Entidades.Direccion();
+
+                cliente.Direccion.Avenida = _vista.CalleAvenidaCliente.Text;
+
+                cliente.Direccion.Urbanizacion = _vista.UrbanizacionCliente.Text;
+
+                cliente.Direccion.Edif_Casa = _vista.EdificioCasaCliente.Text;
+
+                cliente.Direccion.Oficina = _vista.PisoApartamentoCliente.Text;
+
+                cliente.Direccion.Ciudad = _vista.CiudadCliente.Text;
+
+                cliente.AreaNegocio = _vista.AreaNegocioCliente.Text;
+
+                //cliente.Telefono.Numero = int.Parse((string)_vista.TelefonoTrabajoCliente.Text);
+
+                //cliente.Telefono.Codigoarea = int.Parse((string)_vista.CodigoTrabajoCliente.Text);
+
+                //cliente.Telefono.Tipo = _vista.TipoTelefono.Text;
+
+                Ingresar(cliente);
+
+                limpiarRegistro();
             }
-            #endregion
-
-            #region metodos
-
-            public void IngersarCliente()
+            catch (WebException e)
             {
-                Core.LogicaNegocio.Entidades.Cliente cliente = new Core.LogicaNegocio.Entidades.Cliente();
-                try 
-                {
-                    cliente.Direccion = new Direccion();
-                    
-                    //cliente.Telefono = new TelefonoTrabajo();
-                    
-                    cliente.Rif = _vista.rifCliente.Text;
-                    
-                    cliente.Nombre = _vista.NombreCliente.Text;
-                    
-                    cliente.Direccion.Calle = _vista.CalleAvenidaCliente.Text;
-                    
-                    cliente.Direccion.Urbanizacion = _vista.UrbanizacionCliente.Text;
-                    
-                    cliente.Direccion.Edif_Casa = _vista.EdificioCasaCliente.Text;
-                    
-                    cliente.Direccion.Piso_apto = _vista.PisoApartamentoCliente.Text;
-                    
-                    cliente.Direccion.Ciudad = _vista.CiudadCliente.Text;
-                    
-                    cliente.AreaNegocio = _vista.AreaNegocioCliente.Text;
-                    
-                    //cliente.Telefono.Numero = int.Parse((string)_vista.TelefonoTrabajoCliente.Text);
-                    
-                    //cliente.Telefono.Codigoarea = int.Parse((string)_vista.CodigoTrabajoCliente.Text);
-
-                    //cliente.Telefono.Tipo = _vista.TipoTelefono.Text;
-                    
-                    Ingresar(cliente);
-                    
-                    limpiarRegistro();                                                                                                   
-                }
-                catch(WebException e)
-                {
-
-                }
 
             }
-            public void limpiarRegistro ()
+
+        }
+        public void limpiarRegistro()
         {
             _vista.AreaNegocioCliente.Text = campoVacio;
             _vista.CalleAvenidaCliente.Text = campoVacio;
-            _vista.CiudadCliente.Text = campoVacio;          
+            _vista.CiudadCliente.Text = campoVacio;
             _vista.EdificioCasaCliente.Text = campoVacio;
             _vista.NombreCliente.Text = campoVacio;
             _vista.PisoApartamentoCliente.Text = campoVacio;
@@ -82,25 +84,25 @@ namespace Presentador.Cliente.Vistas
             _vista.UrbanizacionCliente.Text = campoVacio;
             _vista.TelefonoTrabajoCliente.Text = campoVacio;
             _vista.CodigoTrabajoCliente.Text = campoVacio;
-                                    
+
         }
 
-            #endregion
-            #region Comando
+        #endregion
+        #region Comando
 
         public void Ingresar(Core.LogicaNegocio.Entidades.Cliente cliente)
         {
             Core.LogicaNegocio.Comandos.ComandoCliente.Ingresar ingresar;
-            
-            ingresar= Core.LogicaNegocio.Fabricas.FabricaComandosCliente.CrearComandoIngresar(cliente);
+
+            ingresar = Core.LogicaNegocio.Fabricas.FabricaComandosCliente.CrearComandoIngresar(cliente);
 
             ingresar.Ejecutar();
 
         }
 
         #endregion
-            
-        }
-       
+
     }
+
+}
 
