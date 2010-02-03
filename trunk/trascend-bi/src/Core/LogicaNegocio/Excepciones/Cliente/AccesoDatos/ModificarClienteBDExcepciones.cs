cf28f1fe0a2e30881121ca4f41e6d6830128e0d5
jesusviloriag <jesusviloriag@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-02-03 09:12:42 +0000

    No se subio este archivo

---
+++ trunk/trascend-bi/src/Core/LogicaNegocio/Excepciones/Cliente/AccesoDatos/ModificarClienteBDExcepciones.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.LogicaNegocio.Excepciones.Cliente.AccesoDatos
+{
+    public class ModificarClienteBDExcepciones : ModificarException
+    {
+        public ModificarClienteBDExcepciones()
+        {
+
+        }
+
+        public ModificarClienteBDExcepciones(string s, Exception e)
+            : base(s, e)
+        {
+
+        }
+    }
+}
