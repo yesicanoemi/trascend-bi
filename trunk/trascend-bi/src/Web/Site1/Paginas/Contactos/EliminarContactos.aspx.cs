2dc5ac2a8f8a2c678747367feff067a103c459e2
trejo.da <trejo.da@ecf31d62-e5b1-11de-8dea-4d08a2fd72f5>
2010-02-03 03:30:33 +0000

    Desarrollo 3
    Cambios para usuario

--- trunk/trascend-bi/src/Web/Site1/Paginas/Contactos/EliminarContactos.aspx.cs
+++ trunk/trascend-bi/src/Web/Site1/Paginas/Contactos/EliminarContactos.aspx.cs
@@ -250,23 +250,32 @@ public partial class Paginas_Contactos_EliminarContactos : PaginaBase, IEliminar
 
         bool permiso = false;
 
-        for (int i = 0; i < usuario.PermisoUsu.Count; i++)
+        try
         {
-            if (usuario.PermisoUsu[i].IdPermiso == 10)
+            for (int i = 0; i < usuario.PermisoUsu.Count; i++)
             {
-                i = usuario.PermisoUsu.Count;
+                if (usuario.PermisoUsu[i].IdPermiso == 10)
+                {
+                    i = usuario.PermisoUsu.Count;
 
-                _presentador = new EliminarPresentador(this);
+                    _presentador = new EliminarPresentador(this);
 
-                permiso = true;
+                    permiso = true;
 
+                }
+            }
+
+            if (permiso == false)
+            {
+                Response.Redirect(paginaSinPermiso);
             }
         }
-
-        if (permiso == false)
+        catch (Exception a)
         {
-            Response.Redirect(paginaSinPermiso);
+            Response.Redirect(paginaDefault);
+
         }
+
     }
 
     protected void uxRbCampoBusqueda_SelectedIndexChanged(object sender, EventArgs e)
